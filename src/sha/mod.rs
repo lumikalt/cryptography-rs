bf908520728c6d27c1782062964d8ebf5cf42cd8
Lumi Kalt <lumi.mia.kalt@gmail.com>
2022-12-17 15:57:34 +0000

    QOL function to print the hash

--- src/sha/mod.rs
+++ src/sha/mod.rs
@@ -1,2 +1,20 @@
+use crate::sha::sha512::Sha512;
+
+use self::sha1::Sha1;
+
 pub mod sha1;
 pub mod sha512;
+
+pub fn print_digest(sha: u16, msg: &str) -> String {
+    match sha {
+        1 => Sha1::new(msg.as_bytes())
+            .result()
+            .iter()
+            .fold("".to_string(), |acc, x| format!("{acc}{:x}", *x)),
+        512 => Sha512::new(msg.as_bytes())
+            .result()
+            .iter()
+            .fold("".to_string(), |acc, x| format!("{acc}{:x}", *x)),
+        _ => panic!("Not yet implemented!"),
+    }
+}
